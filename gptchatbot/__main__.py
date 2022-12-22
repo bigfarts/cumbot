8fec878e8a21263eb6aeb7d1acfb06620b3ee051
bigfarts <bigfarts@punymail.com>
2022-12-22 00:41:21 -0800

    add main

---
+++ gptchatbot/__main__.py
@@ -0,0 +1,17 @@
+import argparse
+
+from .bot import run_bot
+from .backends import openai
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--discord-token", required=True)
+    parser.add_argument("--openai-token", required=True)
+    args = parser.parse_args()
+
+    run_bot(args.discord_token, openai.Backend(args.openai_token))
+
+
+if __name__ == "__main__":
+    main()
